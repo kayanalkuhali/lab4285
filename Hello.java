265284dbe41950e86bb6590f281640a1164a23f1
kayan <alkuhali@umich.edu>
2021-10-07 10:08:05 -0400

    loop2

--- Hello.java
+++ Hello.java
@@ -1,1 +1,17 @@
-public class Hello {public static void main(String[] args) {for(int i=1;i<=15;i++) {System.out.println("Hello World..."+i);}}}
+public class Hello
+
+{
+  public static void main(String[] args)
+
+   {
+     for(int i=1;i<=20;i++) 
+
+
+
+     {
+       System.out.println("Hello World..."+i);
+
+
+   }
+ }
+}
